c97ffc97d493e9802cac69211e7e09d1c7b11c7d
gabrielsparreira <42394662+gabrielsparreira@users.noreply.github.com>
2021-06-30 11:55:39 -0300

    Update teste3dArray.c

--- prototype/teste3dArray.c
+++ prototype/teste3dArray.c
@@ -3,15 +3,30 @@ int main()
 {
 int c[2][4][3];
 int i,j,k;
+  
 printf("Enter elements into 3-D array: ");
 for(i=0;i<2;i++)
 {
-for(j=0;j<4;j++)
+  for(j=0;j<4;j++)
+  {
+    for(k=0;k<3;k++)
+    {
+    scanf("%d",&c[i][j][k]);
+    }
+  }
+}
+  for(i=0;i<2;i++)
 {
-for(k=0;k<3;k++)
-{
-scanf("%d",&c[i][j][k]);
+	for(j=0;j<4;j++)
+	{
+		for(k=0;k<3;k++)
+		{
+		printf("\t%d",c[i][j][k]);
+		}
+	printf("\n");
+	}
+printf("\n");
 }
-}
-}
+getch();
+return 0;
 }
