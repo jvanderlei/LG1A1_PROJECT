3b0b4ea7ba0a6c11ee93209041642fcb5f167a8e
gabrielsparreira <42394662+gabrielsparreira@users.noreply.github.com>
2021-07-08 09:37:43 -0300

    Update functions.h

--- functions.h
+++ functions.h
@@ -82,12 +82,16 @@ void cadastrar() { // CADASTRAR  // CADASTRAR  // CADASTRAR  // CADASTRAR  // CA
 	
 	printf(" Aqui você poderá registrar um novo questionário de acordo com a sua preferência. \n");
 	
+	// Registrando o nome do questionário para uso nas funções realizar() e visualizar();
 	FILE *questRegister, *questNovo;
 	questRegister = fopen("questRegistrados.txt", "a");
 	
 	printf(" Digite o nome do questionário: ");
 	gets(buffer1);
+	// localeAcc() é uma função que fiz para consertar a acentuação por conta de uma incompatibilidade entre a locale.h e a stdio.h;
 	localeAcc(buffer1);
+	replacestr(buffer1, " ", "_"); // Subst. " " por "_" para facilitar o acesso;
+	strncat(buffer1, ".txt", 4); // Concatena ".txt" ao fim da string buffer1 que contém o nome do arquivo;
 
 	printf(" O nome %s foi registrado...\n", buffer1);
 	
@@ -95,6 +99,7 @@ void cadastrar() { // CADASTRAR  // CADASTRAR  // CADASTRAR  // CADASTRAR  // CA
 	
 	fclose(questRegister);
 	
+	//Definição de alguns parâmetros (qtd de frases/perguntas e itens resposta por frase) pelo usuário para customização do questionário;
 	printf(" Informe quantas frases o questionário terá: ");
 	scanf(" %d", &qtFrases);
 	
@@ -102,12 +107,12 @@ void cadastrar() { // CADASTRAR  // CADASTRAR  // CADASTRAR  // CADASTRAR  // CA
 	scanf(" %d", &qtItens);
 	fflush(stdin);
 	
-	replacestr(buffer1, " ", "_");
-	strncat(buffer1, ".txt", 4);	
+	// Criação do arquivo do questionário
 	questNovo = fopen(buffer1, "w");
 	
 	char quest[qtFrases+1][qtItens+2][255];
 	
+	// Definição da primeira linha da matriz "quest[][][]", que será padronizada;
 	strcpy(quest[0][0], "NroFrase");
 	strcpy(quest[0][1], "Frase");
 	for (j=2; j<qtItens+2; j++) {
@@ -119,53 +124,54 @@ void cadastrar() { // CADASTRAR  // CADASTRAR  // CADASTRAR  // CADASTRAR  // CA
 		printf("quest[%d][0] : %s\n", i, quest[i][0]);
 	}
 	
-		for (i=1; i<qtFrases+1; i++) {
-			for (j=1; j<qtItens+2; j++) {
-				if (j!=1) {
-					printf(" Informe o %do Item da %da Frase: \n", j-1, i);
-					gets(quest[i][j]);
-					localeAcc(quest[i][j]);
-				} else {
-					printf(" Informe a %da Frase: \n", i);
-					gets(quest[i][j]);
-					localeAcc(quest[i][j]);
-				}
+	// Input das frases e de seus itens (as demais linhas);
+	for (i=1; i<qtFrases+1; i++) {
+		for (j=1; j<qtItens+2; j++) {
+			if (j!=1) {
+				printf(" Informe o %do Item da %da Frase: \n", j-1, i);
+				gets(quest[i][j]);
+				localeAcc(quest[i][j]);
+			} else {
+				printf(" Informe a %da Frase: \n", i);
+				gets(quest[i][j]);
+				localeAcc(quest[i][j]);
 			}
 		}
+	}
 	
-	for (i=0; i<qtFrases+1; i++) {
-		
-		
-		for (j=0; j<qtItens+2; j++) {
-			
+	// Registro das informações inputadas no arquivo criado;
+	for (i=0; i<qtFrases+1; i++) {		
+		for (j=0; j<qtItens+2; j++) {			
 							
 				fprintf(questNovo, "%s", quest[i][j]);
 				
-				for (k=0; k<qtFrases+1; k++) {
-					
-					if (strlen(quest[k][j]) >= aux1) {
-						
-						aux1 = strlen(quest[k][j]);
-						
+				// Formatação de espaços no arquivo;
+					for (k=0; k<qtFrases+1; k++) {					
+						if (strlen(quest[k][j]) >= aux1) {						
+							aux1 = strlen(quest[k][j]);						
+						}
 					}
-					
-				}
-				aux1 = aux1 - strlen(quest[i][j]);
-				for (k=0; k<aux1; k++) {
-					fprintf(questNovo, " ");
-				}
-				
+
+					aux1 = aux1 - strlen(quest[i][j]);
+
+					for (k=0; k<aux1; k++) {
+						fprintf(questNovo, " ");
+					}
+			
+			// Formatação das vírgulas para leitura do arquivo;	
 			if (j < qtItens+1) {
 				
 				fprintf(questNovo, " , ");
 				
 				}
+			// Resetando aux1 para que seja usado quando a formatação da próxima coluna comece;
 			aux1 = 0;
-			}	
-			
-			if (i<qtFrases) {
-				fprintf(questNovo, "\n");
-			}
+		}	
+		
+		//Fim da linha gera um \n
+		if (i<qtFrases) {
+			fprintf(questNovo, "\n");
+		}
 	}
 	
 	fclose(questNovo);
@@ -223,53 +229,74 @@ int replacestr(char *line, const char *search, const char *replace) { // REPLACE
    return(count);
 }
 
-int localeAcc(char *line) // LOCALE.H FIX  // LOCALE.H FIX  // LOCALE.H FIX  // LOCALE.H FIX  // LOCALE.H FIX  // LOCALE.H FIX  // LOCALE.H FIX  // LOCALE.H FIX  // LOCALE.H FIX  // LOCALE.H FIX  // LOCALE.H FIX  // LOCALE.H FIX  //
+int localeAcc(char *line) //  LOCALE ACC  //  LOCALE ACC  //  LOCALE ACC  //  LOCALE ACC  //  LOCALE ACC  //  LOCALE ACC  //  LOCALE ACC  //  LOCALE ACC  //  LOCALE ACC  //  LOCALE ACC  //  LOCALE ACC  // 
 {
 	int i;
 	int aux = strlen(line);
 	for (i=0; i<aux; i++) {
 		switch ((int)line[i]) {
-
+			
+			// fix para à
 			case -123 : {
 				line[i] = 224;
 				break;
 			}
+				
+			//fix para á
 			case -96 : {
 				line[i] = 225;
 				break;
 			}
+				
+			// fix para â
 			case -125 : {
 				line[i] = 226;
 				break;
 			}
+				
+			// fix para ç
 			case -121 : {
 				line[i] = 231;
 				break;
 			}
+				
+			// fix para é
 			case -126 : {
 				line[i] = 233;
 				break;
 			}
+				
+			// fix para ê
 			case -120 : {
 				line[i] = 234;
 				break;
 			}
+				
+			// fix para í
 			case -95 : {
 				line[i] = 237;
 				break;
 			}
+				
+			// fix para ó
 			case -94 : {
 				line[i] = 243;
 				break;
 			}
+				
+			// fix para ô
 			case -109 : {
 				line[i] = 244;
 				break;
 			}
+				
+			// fix para ú
 			case -93 : {
 				line[i] = 250;
 				break;
 			}
+				
+			// fix para É
 			case -112 : {
 				line[i] = 201;
 				break;
